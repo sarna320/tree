9e2a9eebce7dd9c12477fa83104e625d4c1225e5
sarna320 <sarnacki99@wp.pl>
2023-12-07 02:16:48 +0100

    Adde tests

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ from test import *
 
 # Autor Pawel Sarnacki
 def main():
-    list_entry = [] # list with data to test
+    list_entry = []  # list with data to test
     for i in range(0, 10000):
         list_entry.append(random.randint(1, 30000))
 
-    number_of_data = list(range(1000, 11000, 1000)) # list of how many data will be tested from list of entry
+    number_of_data = list(
+        range(1000, 11000, 1000)
+    )  # list of how many data will be tested from list of entry
     time_of_build_AVL = []
     time_of_search_AVL = []
     for i in number_of_data:
@@ -30,8 +32,6 @@ def main():
 
     draw_plots(time_of_build_AVL, number_of_data, "build AVL")
     draw_plots(time_of_search_AVL, number_of_data, "search AVL")
-
-    test_AVL()
 
 
 if __name__ == "__main__":
