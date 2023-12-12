6c09cfc386a3ac7b5a5c58dd46aa8c5e370c53f5
niotrek <piotr.niedzialek@protonmail.com>
2023-12-12 19:03:43 +0100

    a

--- draw_plots.py
+++ draw_plots.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 
 # Autor Pawel Sarnacki
-def draw_plots(times, number_data, what=""):
+def draw_one_plot(times, number_data, what=""):
     ax = plt.subplots()[1]
     ax.plot(number_data, times)
     ax.set_xlabel("Number of entry data")
@@ -10,4 +10,16 @@ def draw_plots(times, number_data, what=""):
     # ax.legend()
     ax.set_title(f"For {what} operations")
     plt.savefig("plots/" + f"{what}" + ".png")
+    plt.show()
+
+# Autor Piotr Niedziałek
+def draw_plots(times1, times2, number_data, label1="", label2=""):
+    fig, ax = plt.subplots()
+    ax.plot(number_data, times1, label=label1)
+    ax.plot(number_data, times2, label=label2)
+    ax.set_xlabel("Number of entry data")
+    ax.set_ylabel("Time [s]")
+    ax.legend()
+    ax.set_title("For operations")
+    plt.savefig("plots/" + f"{label1}_{label2}" + ".png")
     plt.show()
--- main.py
+++ main.py
@@ -17,6 +17,10 @@ def main():
     )  # list of how many data will be tested from list of entry
     time_of_build_AVL = []
     time_of_search_AVL = []
+    time_of_build_BST = []
+    time_of_search_BST = []
+    time_of_delete_BST = []
+
     for i in number_of_data:
         avl_tree = AVLTree()
         start = time.process_time()
@@ -29,9 +33,28 @@ def main():
             avl_tree.search_key(data)
         stop = time.process_time()
         time_of_search_AVL.append(stop - start)
+        #BST
+        bst_tree = BST()
+        start = time.process_time()
+        for data in list_entry[0:i]:
+            bst_tree.insert(data)
+        stop = time.process_time()
+        time_of_build_BST.append(stop - start)
+        start = time.process_time()
+        for data in list_entry[0:i]:
+            bst_tree.search(data)
+        stop = time.process_time()
+        time_of_search_BST.append(stop - start)
+        start = time.process_time()
+        for data in list_entry[0:i]:
+            bst_tree.delete(data)
+        stop = time.process_time()
+        time_of_delete_BST.append(stop - start)
 
-    draw_plots(time_of_build_AVL, number_of_data, "build AVL")
-    draw_plots(time_of_search_AVL, number_of_data, "search AVL")
+
+    draw_plots(time_of_build_AVL, time_of_build_BST, number_of_data, "build AVL", "build BST")
+    draw_plots(time_of_search_AVL, time_of_search_BST, number_of_data, "search AVL", "search BST")
+    draw_one_plot(time_of_delete_BST, number_of_data, "delete BST")
 
 
 if __name__ == "__main__":
