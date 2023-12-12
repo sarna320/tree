421efb995914bf00ed7736317fa9795777198222
niotrek <piotr.niedzialek@protonmail.com>
2023-12-12 17:49:47 +0100

    added bst

---
+++ BST.py
@@ -0,0 +1,62 @@
+from Node import *
+
+
+# Autor Piotr Niedziałek
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        if self.root is None:
+            self.root = Node(key)
+        else:
+            self._insert(self.root, key)
+
+    def _insert(self, node, key):
+        if key < node.key:
+            if node.left is None:
+                node.left = Node(key)
+            else:
+                self._insert(node.left, key)
+        elif key > node.key:
+            if node.right is None:
+                node.right = Node(key)
+            else:
+                self._insert(node.right, key)
+
+    def search(self, key):
+        return self._search(self.root, key)
+
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node
+        if key < node.key:
+            return self._search(node.left, key)
+        return self._search(node.right, key)
+
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
+
+    def _delete(self, node, key):
+        if node is None:
+            return node
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            temp = self._minValueNode(node.right)
+            node.key = temp.key
+            node.right = self._delete(node.right, temp.key)
+        return node
+
+    def _minValueNode(self, node):
+        current = node
+        while(current.left is not None):
+            current = current.left
+        return current
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from AVL import *
 from draw_graphs import *
+from BST import *
 
 
 # Autor Pawel Sarnacki
@@ -42,8 +43,44 @@ def test_AVL():
     ] == avl_tree_test.get_all_balance_factors():
         print("Balance for inorder traversal:", avl_tree_test.get_all_balance_factors())
         print("Test for balance inorder traversal AVL passed")
-    draw_graph("AVL", avl_tree_test.get_edges_list())
+    #draw_graph("AVL", avl_tree_test.get_edges_list())
+
+#Autor Piotr Niedziałek
+def test_bst():
+    bst = BST()
+
+    # Test insert
+    bst.insert(23)
+    bst.insert(15)
+    bst.insert(34)
+    bst.insert(51)
+    bst.insert(12)
+    assert bst.root.key == 23
+    assert bst.root.right.key == 34
+    assert bst.root.left.key == 15
+    assert bst.root.left.left.key == 12
+    assert bst.root.right.right.key == 51
+
+    # Test search
+    assert bst.search(23).key == 23, "Test failed: 23 not found"
+    assert bst.search(34).key == 34, "Test failed: 34 not found"
+    assert bst.search(15).key == 15, "Test failed: 15 not found"
+    assert bst.search(12).key == 12, "Test failed: 12 not found"
+    assert bst.search(51).key == 51, "Test failed: 51 not found"
+    assert bst.search(60) is None, "Test failed: 60 found"
+
+
+    # Test delete
+    bst.delete(23)
+    assert bst.search(23) is None
+    assert bst.root.key == 34
+  
+
+    print("All tests passed!")
+
+
 
 
 if __name__ == "__main__":
     test_AVL()
+    test_bst()
